23a1b53fd61b1b3468d679ae7ae88b3cd0a57d17
oikuman <oikuma@gmail.com>
2019-10-15 00:08:54 +0300

    Updates

---
+++ precache-manifest.9a47e291e101fecaa18c358b8d6a2132.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e312b7b1eaaf1046f8baf7d0222c2c3f",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "68a8f67a87f68f2e68dc",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.66c742fe.chunk.css"
+  },
+  {
+    "revision": "dfb329bfc6113abb2d5c",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.d24ef433.chunk.js"
+  },
+  {
+    "revision": "68a8f67a87f68f2e68dc",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.e526f036.chunk.js"
+  },
+  {
+    "revision": "341189b882990fcbc256",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.f5211a8c.js"
+  }
+]);
